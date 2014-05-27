a1e3b8f7afe43666dabf5e4b8666ccd69874c8c5
Jarl André Hübenthal <jarl.andre@joint.no>
2014-05-27 22:54:24 +0200

    Fixing stupid sql script

--- blog.sql
+++ blog.sql
@@ -1,16 +1,16 @@
 CREATE TABLE `entries` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(45) DEFAULT NULL,
-  `content` longtext,
+  `title` varchar(45) NOT NULL,
+  `content` longtext NOT NULL,
   `publishedDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `authorId` varchar(45) DEFAULT NULL,
+  `authorId` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `users` (
   `id` varchar(45) NOT NULL,
-  `name` varchar(45) DEFAULT NULL,
-  `pass` varchar(100) DEFAULT NULL,
+  `name` varchar(45) NOT NULL,
+  `pass` varchar(100) NOT NULL,
   `active` tinyint default 0,
   `level` int default 1,
   PRIMARY KEY (`id`)
